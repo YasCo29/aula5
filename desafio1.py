c584f674ef5898000686d39326e5b56050b46fd0
YasCo29 <yasmincalmeida2907@gmail.com>
2024-11-30 17:55:03 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,19 @@
+#Desafio:  Crie um sistema de e-commerce, onde o usuário possa: Utilize variáveis, listas, condicionais
+
+#Foco resolver o problema
+
+#Vamos trabalhar:
+#Criatividade | Pensamento lógico | Flexibilidade cognitiva
+#• cadastrar-se
+#• comprar um produto
+#• descobrir o valor total
+#• pagar
+
+print('bem -vindo(a)!')
+print()
+print('Por gentileza,preencha os dados abaixo')
+ nome = int(input('Nome:'))
+data de nascimento = int(input('data de nascimento:'))
+endereço = int(input('endereço:'))
+
+#finaliza########
